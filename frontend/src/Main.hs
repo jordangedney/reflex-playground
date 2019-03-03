107299b4815e4533071eb90dcbed9c7f26513eba
Jordan Gedney <jordangedney@gmail.com>
2019-03-03 14:18:29 -0500

    Futzing with tracing

--- frontend/src/Main.hs
+++ frontend/src/Main.hs
@@ -9,9 +9,10 @@
 #-}
 module Main (main) where
 
-import Language.Javascript.JSaddle.Warp (run)
-import Reflex.Dom (MonadWidget, (=:))
-import Reflex.Dom.Core (mainWidget, mainWidgetWithCss, mainWidgetWithHead)
+import qualified Language.Javascript.JSaddle.Warp as JSW
+import Reflex.Dom (MonadWidget, (=:), (&), (.~))
+import qualified Reflex.Dom.Core as RDC
+-- (mainWidget, mainWidgetWithCss, mainWidgetWithHead)
 import JSDOM.CanvasRenderingContext2D (CanvasRenderingContext2D)
 import JSDOM.Types (JSM, JSString, liftJSM)
 
@@ -20,24 +21,24 @@ import Squares (squaresApp)
 import DrawM (DrawM, runDrawM)
 import Style (css, blackBkground)
 
+import Reflex.Dom
 import qualified Reflex.Dom as RD
 import qualified Data.Text as T
 import qualified Data.Map as Map
 import Data.Monoid ((<>))
 import Data.FileEmbed
 import Control.Monad.Fix (MonadFix)
-
-
---  https://github.com/hansroland/reflex-dom-inbits/blob/master/tutorial.md
--- Text Input Fields
-
+import Data.Maybe (fromJust, fromMaybe)
+import Data.Time
+import Control.Monad.Trans (liftIO)
 
 main :: IO ()
-main = run 3911 $ mainWidgetWithHead headElement bodyElement
+main = JSW.run 3911 $ RDC.mainWidgetWithHead headElement bodyElement
+-- main = mainWidgetWithHead headElement bodyElement
 
 headElement :: MonadWidget t m => m ()
 headElement = do
-  RD.el "title" $ RD.text "Main Title"
+  RD.el "title" $ RD.text "Playground"
   styleSheet "css/simple.css"
   where
     styleSheet link = RD.elAttr "link" (Map.fromList [
@@ -47,20 +48,56 @@ headElement = do
       ]) $ return ()
     css = $(embedFile "css/simple.css")
 
+--     numbs <- (0 :: Int) <| [(+ 1) <$ evIncr,
+--                             (+ (-1)) <$ evDecr,
+--                             const 0 <$ evReset]
+--       RD.el "div" $ RD.display numbs
 -- | Event Map
 (<|) :: (Control.Monad.Fix.MonadFix m, RD.MonadHold t m, RD.Reflex t)
      => a -> [RD.Event t (a -> a)] -> m (RD.Dynamic t a)
 initialValue <| eventFns = do
   RD.foldDyn ($) initialValue $ RD.leftmost eventFns
 
+debug = True
+
+tE e = tE' "" e
+
+tE' :: (Monad m, Reflex t, Show a)
+    => T.Text -> m (Event t a) -> m (Event t a)
+tE' s e =
+  if debug then do
+    event <- e
+    let tracedEvent = traceEvent (T.unpack s) event
+    return tracedEvent
+  else e
+
+tEW :: (Monad m, Reflex t)
+    => T.Text -> m (Event t a) -> m (Event t a)
+tEW s e =
+  if debug then do
+    event <- e
+    let tracedEvent = traceEventWith (\_ -> T.unpack s) event
+    return tracedEvent
+  else e
+
 bodyElement :: MonadWidget t m => m ()
-bodyElement = do
-  rec RD.el "h2" $ RD.text "Counter as a fold"
-      numbs <- (0 :: Int) <| [(+ 1) <$ evIncr,
-                              (+ (-1)) <$ evDecr,
-                              const 0 <$ evReset]
-      RD.el "div" $ RD.display numbs
-      evIncr <- RD.button "Increment"
-      evDecr  <- RD.button "Dec"
-      evReset  <- RD.button "Reset"
+bodyElement  = el "div" $ do
+  el "h2" $ text "Swiss Meteo Data (raw version)"
+  text "Choose station: "
+  dd <- dropdown "BER" (constDyn stations) def
+  -- Build and send the request
+  evStart <- tE getPostBuild
+  let evCode = tagPromptlyDyn (value dd) $
+        leftmost [ () <$ (tE _dropdown_change dd), evStart]
+  evRsp <- tEW "Received query result" (performRequestAsync $ buildReq <$> evCode)
+  -- Display the whole response
+  el "h5" $ text "Response Text:"
+  let evResult = (fromMaybe "" . _xhrResponse_responseText) <$> evRsp
+  dynText =<< holdDyn "" evResult
   return ()
+
+buildReq :: T.Text -> XhrRequest ()
+buildReq code = XhrRequest "GET" ("https://opendata.netcetera.com/smn/smn/" <> code) def
+
+stations :: Map.Map T.Text T.Text
+stations = Map.fromList [("BIN", "Binn"), ("BER", "Bern"), ("KLO", "Zurich airport"), ("ZER", "Zermatt"), ("JUN", "Jungfraujoch"), ("ARB", "ANN ARBOR")]
