f48bad7001f22852b716795147d42e599ef2c43a
Jordan Gedney <jordangedney@gmail.com>
2019-03-20 23:37:31 -0400

    randomish pathfinding

--- frontend/src/RandomFloodFill.hs
+++ frontend/src/RandomFloodFill.hs
@@ -122,22 +122,31 @@ startingCoords maxWidth maxHeight rnd = do
 coordsToVisit :: Integer -> Integer -> IO Integer -> IO [(Integer, Integer)]
 coordsToVisit maxWidth maxHeight rnd = do
   (x, y) <- liftIO $ startingCoords maxWidth maxHeight rnd
-  let rest = toVisit (\x -> inBounds x maxWidth maxHeight) [] [(x,y)]
+  let rest = toVisit (\x -> inBounds x maxWidth maxHeight) [] [(x,y)] [1..]
   pure $ [(x, y)] ++ takeWhile (\coord -> inBounds coord maxWidth maxHeight) rest
 
-popRandomNode (x:xs) = (x, xs)
+
+removeIndex :: Int -> [a] -> [a]
+removeIndex ind xs = (take (ind - 1) xs) ++ (drop ind xs)
+
+popRandomNode :: Integer -> [a] -> (a, [a])
+popRandomNode rnd (x:[]) = (x, [])
+popRandomNode rnd xs = ((xs !! removed'), removeIndex removed' xs)
+  where removed' = ((fromIntegral rnd) + 1) `mod` (length xs)
+        removed = if removed == 0 then 0 else removed' - 1
 
 type Coord = (Integer, Integer)
 
-toVisit :: (Coord -> Bool) -> [Coord] -> [Coord] -> [Coord]
-toVisit inbounds visited frontier = toVisit'
-  where (nextNode, frontierWithout) = popRandomNode frontier
+toVisit :: (Coord -> Bool) -> [Coord] -> [Coord] -> [Integer] -> [Coord]
+toVisit inbounds visited frontier (r:randomIndexes) = toVisit'
+  where (nextNode, frontierWithout) = popRandomNode r frontier
         nextNeighbors = filter (\x -> inbounds x && unseenNode x) $ neighbors nextNode
         unseenNode node = not (node `elem` visited || node `elem` frontier)
         newFrontier = frontierWithout ++ nextNeighbors
         toVisit' = case newFrontier of
                      [] -> [nextNode]
-                     _ -> nextNode:(toVisit inbounds (nextNode:visited) newFrontier)
+                     _ -> nextNode:(
+                       toVisit inbounds (nextNode:visited) newFrontier randomIndexes)
 
 neighbors :: (Integer, Integer) -> [(Integer, Integer)]
 neighbors (x, y) = [(x-1, y), (x, y - 1), (x+1, y), (x, y+1)]
